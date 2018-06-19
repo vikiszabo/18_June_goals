345a5797809aa5be8ed2fc14c9c32f7e521bce47
weiliya <viktoria.szabo.cv@gmail.com>
2018-06-19 08:09:24 +0200

    solved as3

--- sitwplans/algo/as3.py
+++ sitwplans/algo/as3.py
@@ -12,3 +12,30 @@ Output: "Mr%20John%20Smith
 
 '''
 
+def replace_spaces_to_string(str, n):
+    str = str.strip()
+    new_string = str.replace(' ', '%20')
+
+    return new_string
+
+
+str1 = "Mr John Smith          "
+n1 = 13
+
+print(replace_spaces_to_string(str1, n1))
+
+
+def replace_spaces(str):
+    res = []
+    start = False
+    str = str.strip()
+    for char in reversed(str):
+        if(char == ' '):
+            res += '02%'
+        else:
+            res += char
+    res = ''.join(res[::-1])
+    return res
+
+print(replace_spaces(str1))
+
